94f08dc9fea3fd751f5001dc570189cea9db2320
kiranmai puvulla <108078968+kiranmaiep@users.noreply.github.com>
2025-09-11 16:44:49 +0530

    Update Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     System.out.println("Hello from Pipeline CI!");
     System.out.println("updated to trigger automatically");
     System.out.println("checking again");
+System.out.println("checking webhook");
+
   }
 }
